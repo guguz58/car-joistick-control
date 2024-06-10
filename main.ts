46094c35b942a8b098b021c67d4b9258b077464c
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2024-06-10 14:26:40 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -36,7 +36,7 @@ radio.onReceivedValue(function (name, value) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         } else {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         }
     }
@@ -46,7 +46,7 @@ radio.onReceivedValue(function (name, value) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         } else {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
         }
     }
     if (name == "16") {
